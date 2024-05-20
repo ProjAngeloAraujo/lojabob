3e7d4c5f41669f5e9e86407b739d319c7a5269aa
ProjAngeloAraujo <167815974+ProjAngeloAraujo@users.noreply.github.com>
2024-05-20 18:24:14 -0300

    Add files via upload

--- rotas.js
+++ rotas.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const app = express()
 
-const AlunoBD = require('./app')
+const AlunoBD = require('./app2')
+
+app.get('/usuarios', function(req,res){
+    res.type("usuarios.html")
+})
+
+//iniciar servidor
+let server = app.listen(3000, function(){
+    let host = server.address().address
+    let port = server.address().port
+    console.log("servidor iniciado em http://%s:%s", host, port)
+})
